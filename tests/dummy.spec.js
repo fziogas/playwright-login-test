47cb8417fc6d7c8bb771a67b995fbf3439c87ec7
fziogas <filipposzi91@gmail.com>
2025-06-25 13:35:06 +0300

    Remove dummy.spec.js from repo

--- tests/dummy.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('check login page title', async ({ page }) => {
-  await page.goto('https://practicetestautomation.com/practice-test-login/');
-  const title = await page.title();
-  expect(title).toBe('Test Login | Practice Test Automation');
-});
